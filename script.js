699076d034c4b5c1e33c3b3ab7ba360619e3b45a
name <nameric@ymail.com>
2022-08-18 18:07:11 -0700

    worked to the if statement for past,present, and future.

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 //declare the current day HTML element
-var dayDisplayEl = $("#day-display");
+var currentDayEl = $("#currentDay");
 
 //declare time entries container html element
-var containerEl = $(".container");
+var containerEl = $("#timeEntries");
 
 //declare current hour
-var time = moment().format("hh:mm:ss");
+var currentHour = moment().hour();
 
 //display current time
-function displayDay() {
+function currentDay() {
   var today = moment().format(`dddd MMM DD, YYYY`);
-  dayDisplayEl.text(today);
+  currentDayEl.text(today);
 }
 
 //render a block for each hour of the day
 for (var i = 9; i <= 17; i++) {
-  var key = "hour-" + i;
+  // var key = "hour-" + i;
 
   //attempt to get the saved data for the hour of the loop
 
-  var data = "asf";
+  var data = ""; //fetch from the local storage
 
   //compare i to current hour to determine if this hour is in the past, present, or future
+  var timeClass;
+  //past
+  if (currentHour > i) {
+    timeClass = "past";
+  }
+  //present
+  else if (currentHour === i) {
+    timeClass = "present";
+  }
+  //future
+  else {
+    timeClass = "future";
+  }
+  // if statement for AM else PM
+  const timeSuffix = i < 12 ? "AM" : "PM";
 
   var template = `
   <div class="container">
   <div id="time-9" class="row time-block">
-    <div class="col-md-1 hour">${i}</div>
-    <textarea class="col-md-10 description">${data}</textarea>
-    <button class="btn saveBtn col-md-1">
-      <i class="fas fa-save">${i}</i>
+    <div class="col-md-1 hour">${i < 13 ? i : i - 12}${timeSuffix}</div>
+    <textarea class="col-md-10 description ${timeClass}">${data}</textarea>
+    <button data-hour="${i}" class="btn saveBtn col-md-1">
+      <i class="fas fa-save"></i>
     </button>
   </div>
     `;
 
   // Append the html to the container element
+  containerEl.append(template);
 }
 
 //set up a "click" event listner on the container
@@ -47,5 +63,6 @@ containerEl.on("click", "button", function (event) {
 });
 
 // Save an hour to local storage
+// localStorage.setItem("hour", JSON.stringify(time));
 
-displayDay();
+currentDay();
